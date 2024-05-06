c6b32f98f0f8bd50862cecd3e90dd5de95f89457
Arshia Akbari Alagha <arshia.alagha@gmail.com>
2024-05-06 23:26:37 +0330

    updated triangulated points and used landmarks

--- delaunay.py
+++ delaunay.py
@@ -1,7 +1,6 @@
 import cv2
 import imutils
 import numpy as np
-from scipy.spatial import Delaunay
 from face_recognition import detect_landmarks
 
 
@@ -50,86 +49,74 @@ def delaunay_triangulation(img, points):
         if 0 <= p[0] < img.shape[1] and 0 <= p[1] < img.shape[0]:
             subdiv.insert(p)
 
-    # Allocate space for the triangulated face
-    triangulated_face = np.zeros_like(img)
-
-    # Draw delaunay triangles
-    draw_delaunay(triangulated_face, subdiv, (255, 255, 255))
-
-    return triangulated_face
+    return subdiv
 
 
-def generate_delaunay_triangles(landmarks):
-    # Create Delaunay triangulation from the landmarks
-    tri = Delaunay(landmarks)
-    return tri.simplices
-
-
-def compute_barycenter(triangles, landmarks):
+def compute_barycenter(triangles):
     barycenters = []
     for triangle in triangles:
         # Get coordinates of triangle vertices
-        p1 = landmarks[triangle[0]]
-        p2 = landmarks[triangle[1]]
-        p3 = landmarks[triangle[2]]
+        p1 = (triangle[0], triangle[1])
+        p2 = (triangle[2], triangle[3])
+        p3 = (triangle[4], triangle[5])
         # Compute barycenter
         barycenter = np.mean(np.array([p1, p2, p3]), axis=0)
         barycenters.append(barycenter)
+
     return barycenters
 
 
 def combine_points_with_barycenter(triangles, barycenters):
     combined_points = []
-    for i, triangle in enumerate(triangles):
-        # Get triangle vertices and barycenter
-        p1 = triangle[0]
-        p2 = triangle[1]
-        p3 = triangle[2]
-        barycenter1 = barycenters[i][0]
-        barycenter2 = barycenters[i][1]
+    for triangle, barycenter in zip(triangles, barycenters):
         # Combine points of triangle with barycenter
-        combined_triangle = [p1, p2, p3, barycenter1, barycenter2]
+        combined_triangle = np.concatenate((triangle, barycenter))
         combined_points.append(combined_triangle)
     return combined_points
 
 
-def main():
+def extract_features(img):
+    _, landmarks = detect_landmarks(img)
+    # Compute triangulated face
+    subdiv = delaunay_triangulation(img, landmarks)
+
+    #  Compute barycenter of each triangle
+    barycenters = compute_barycenter(triangles=subdiv.getTriangleList())
+
+    # Combine points of triangles with barycenters
+    combined_points = combine_points_with_barycenter(subdiv.getTriangleList(), barycenters)
+
+    # flatten for our models
+    combined_points = np.array(combined_points).flatten()
+    # print(combined_points.shape)
+    return combined_points, landmarks
+
+
+if __name__ == '__main__':
     # Read the input image
-    img = cv2.imread("Luis.jpg", 1)
+    img = cv2.imread("dataset/fear/S506_004_00000038.png", 1)
 
     # Resize image keeping aspect-ratio to ensure no overflow in visualization
     img = imutils.resize(img, width=800)
 
-    combined_points, landmarks = extract_features(img)
-
-    print(combined_points)
     # Draw face Bounding box
 
     # If l28 only use 28 of the 68 landmarks provided by dlib shape-predictor # l28 = args.l28 #f l28: mask = [0, 2,
     # 4, 6, 8, 10, 12, 14, 16, 18, 20, 21, 22, 23, 25, 27, 29, 30, 31, 35, 36, 39, 42, 45, 48, 51, 54, 57] landmarks
     # = [landmarks[i] for i in mask]
-
+    _, landmarks = detect_landmarks(img)
     # Compute triangulated face
-    img_delaunay = delaunay_triangulation(img, landmarks)
+    subdiv = delaunay_triangulation(img, landmarks)
+
+    # Allocate space for the triangulated face
+    triangulated_face = np.zeros_like(img)
+    # Draw delaunay triangles
+    draw_delaunay(triangulated_face, subdiv, (255, 255, 255))
+    # extract desired features
+    extract_features(img)
     # Show original image and triangulated face
     cv2.imshow('Original Image', img)
-    cv2.imshow('Triangulated Face', img_delaunay)
+    cv2.imshow('Triangulated Face', triangulated_face)
+
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    # Save result in a file
-
-
-def extract_features(img):
-    # Detect landmarks using model
-    _, landmarks = detect_landmarks(img)
-    triangles = generate_delaunay_triangles(landmarks)
-    # Compute barycenter of each triangle
-    barycenters = compute_barycenter(triangles, landmarks)
-    # Combine points of triangles with barycenters
-    combined_points = combine_points_with_barycenter(triangles, barycenters)
-    combined_points = np.reshape(np.array(combined_points), -1)
-    return combined_points, landmarks
-
-
-if __name__ == '__main__':
-    main()
